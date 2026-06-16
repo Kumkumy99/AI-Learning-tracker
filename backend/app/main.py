79ce718e22e1678632fdbd8510096665e00784b4
Kumkum Yadav <yadavkumkum9009@gmail.com>
2026-06-16 23:04:54 +0530

    Added database dependencies

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,18 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends
 from pydantic import BaseModel
-from .database import engine
 from . import models
+from sqlalchemy.orm import Session
+from .database import engine, get_db
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
-goals = []
 class GoalCreate(BaseModel):
     title: str
     completed: bool
+class GoalResponse(BaseModel):
+    id: int
+    title: str
+    completed: bool
+    class Config:
+        from_attributes = True
 @app.get("/")
 def root():
     return {"message": "Welcome to AI Learning Tracker Backend"}
 @app.post("/goals")
-def create_goal(goal: GoalCreate):
-    goals.append(goal.dict())
-    print(goals)
-    return goal
+def create_goal(
+    goal: GoalCreate,
+    db: Session = Depends(get_db)
+):
+    db_goal = models.Goal(
+        title=goal.title,
+        completed=goal.completed
+    )
+    db.add(db_goal)
+    db.commit()
+    db.refresh(db_goal)
+    return db_goal
