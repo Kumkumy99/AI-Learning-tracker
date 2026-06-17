428bac2e066bc41ae111bcdfc7ac2b44f1beb5bb
Kumkum Yadav <yadavkumkum9009@gmail.com>
2026-06-17 20:01:30 +0530

    Added user model

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String, Boolean
-
+from typing import email
 from .database import Base
 
 
@@ -8,3 +8,11 @@ class Goal(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, nullable=False)
     completed = Column(Boolean, default=False)
+
+class User(Base):
+    __tablename__="users"
+    id = Column(Integer, primary_key=True, index=True)
+    username=Column(String,nullable=False)
+    email=Column(String,nullable=False,unique=True,index=True)
+    hashed_password=Column(String,nullable=False)
+
