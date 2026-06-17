0ef791b2dced08e9d04dfeabeb73e546f9af7091
Kumkum Yadav <yadavkumkum9009@gmail.com>
2026-06-17 05:49:44 +0530

    goals table creation cmplete

--- backend/app/routers/goals.py
+++ backend/app/routers/goals.py
@@ -2,13 +2,13 @@ from fastapi import APIRouter,Depends,HTTPException
 from sqlalchemy.orm import Session
 from ..database import get_db
 from .. import models, schemas
-from schemas import GoalCreate,GoalResponse
+from ..schemas import GoalCreate,GoalResponse
 router = APIRouter(
     prefix="/goals",
     tags=["Goals"]
 )
 
-@router.post("/goals")
+@router.post("/")
 def create_goal(
     goal: GoalCreate,
     db: Session = Depends(get_db)
@@ -21,7 +21,7 @@ def create_goal(
     db.commit()
     db.refresh(db_goal)
     return db_goal
-@router.get("/goals", response_model=list[GoalResponse])
+@router.get("/", response_model=list[GoalResponse])
 def get_goals(db: Session = Depends(get_db)):
     goals = db.query(models.Goal).all()
     return goals
