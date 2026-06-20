cece611b7d4f37cc2caddc72f939d07ebd7970d8
Kumkum Yadav <yadavkumkum9009@gmail.com>
2026-06-20 17:30:23 +0530

    login endpointd

--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -48,6 +48,6 @@ def login(user:UserLogin, db: Session = Depends(get_db)):
 }
     access_token = create_access_token(data=token_data)
     return {
-        "access_token": token,
+        "access_token": access_token,
         "token_type": "bearer"
     }
