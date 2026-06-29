49169f6edafe9d5b9decaa5257712344d70c9ffc
Kumkum Yadav <yadavkumkum9009@gmail.com>
2026-06-30 00:18:51 +0530

    Added json response models for ai

--- backend/app/services/ai_service.py
+++ backend/app/services/ai_service.py
@@ -1,4 +1,4 @@
-import os
+import os,json
 from dotenv import load_dotenv
 import google.generativeai as genai
 
@@ -8,11 +8,58 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 genai.configure(api_key=GEMINI_API_KEY)
 model = genai.GenerativeModel("models/gemini-2.5-flash")
 
+def generate_roadmap(goal, ai_input):
+    prompt = f"""
+Generate a learning roadmap in STRICT JSON.
 
-def test_gemini():
-    response = model.generate_content(
-        "Say hello in one sentence"
-    )
-    return response.text
+Goal Title: {goal.title}
+Description: {goal.description}
+Target Date: {goal.target_date}
+
+Skill Level: {ai_input.skill_level}
+Daily Hours: {ai_input.daily_hours}
+Learning Style: {ai_input.learning_style}
+
+Return ONLY valid JSON.
+
+Format:
+{{
+  "phases": [
+    {{
+      "phase_title": "string",
+      "subtasks": [
+        {{
+          "title": "string",
+          "resources": [
+            {{
+              "title": "string",
+              "url": "string",
+              "resource_type": "video/article/practice"
+            }}
+          ]
+        }}
+      ]
+    }}
+  ]
+}}
+"""
+    response = model.generate_content(prompt)
+    print(response.text)
+    clean_text = response.text.replace("```json", "").replace("```", "").strip()
+    roadmap_data = json.loads(clean_text)
+    return roadmap_data
+
+
+class DummyGoal:
+    title = "Learn DSA"
+    description = "For product-based company interviews"
+    target_date = "2026-12-31"
+
+class DummyInput:
+    skill_level = "beginner"
+    daily_hours = 2
+    learning_style = "practice"
 if __name__ == "__main__":
-    print(test_gemini())
+    dummy_goal = DummyGoal()
+    dummy_ai_input = DummyInput()
+    print(generate_roadmap(dummy_goal, dummy_ai_input))
