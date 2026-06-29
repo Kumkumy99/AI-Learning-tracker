492dce5cca6349c289b519a1481bf3419355cce3
Kumkum Yadav <yadavkumkum9009@gmail.com>
2026-06-29 22:09:02 +0530

    added roadmap schema

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -44,3 +44,8 @@ class UserLogin(BaseModel):
 class Token(BaseModel):
     access_token: str
     token_type: str
+
+class GenerateRoadmapRequest(BaseModel):
+    skill_level: str
+    daily_hours: int
+    learning_style: str
